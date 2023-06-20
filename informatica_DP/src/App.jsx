4f7d9946eb7e2a0398fcc9313a7e8628ca9e15ac
david.rizo <david.rizo@desoft.cu>
2023-06-20 15:22:01 -0400

    second commit

--- informatica_DP/src/App.jsx
+++ informatica_DP/src/App.jsx
@@ -1,13 +1,22 @@
 import {BrowserRouter,Route,Routes} from 'react-router-dom';
-import ShowCategoria from './components/ShowCategoria';
+import About from './components/about'
+import Contact from './components/contact'
+import Home from './components/home'
+import NavBarExample from './layouts/navbar'
+
+
+
 function App() {
  
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<ShowCategoria></ShowCategoria>}>
-          
+        <Route path='/' element={<NavBarExample></NavBarExample>}>
+        <Route index element={<Home></Home>}></Route>
+
+          <Route path='about' element={<About></About>}></Route>
+          <Route path='contact' element={<Contact></Contact>}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
