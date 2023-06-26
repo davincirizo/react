04ab315b5bce669299e842e05d2dfddb5cf1e49e
david.rizo <david.rizo@desoft.cu>
2023-06-26 15:01:32 -0400

    second

---
+++ mui_database_axios/src/App.jsx
@@ -0,0 +1,12 @@
+import { TableJson } from './tables/TableJson'
+import { TableBasic } from './tables/TablesBasic'
+
+function App() {
+
+  return (
+    // <TableJson/>
+    <TableBasic/>
+  )
+}
+
+export default App
