2bc7b85b4bc66803e7db87c5f57d6ad1ef3d156c
David <rizod3409@gmail.com>
2023-07-21 07:14:38 -0400

    register

--- crud/src/component/login/Register.jsx
+++ crud/src/component/login/Register.jsx
@@ -2,25 +2,94 @@ import { useForm } from 'react-hook-form'
 import NavBar from '../general/NavBar'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import { useState } from 'react'
+import ColorButtons from '../general/Button'
 
 function Register() {
   const endpoint = 'http://127.0.0.1:8000/api/registro'  
-  const { register , handleSubmit, formState:{errors},watch }= useForm()
+  // const { register , handleSubmit, formState:{errors},watch }= useForm()
   const navigate = useNavigate()
-    
-  const Register = async (e) =>{ 
-    await axios.post(endpoint,{
-          name:e.name,
-          email:e.email,
-          password:e.password,
-          
-      })
-      navigate('/show_category')
+  const [name,setName] = useState('')
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+
+  const Register = async (event) =>{
+    event.preventDefault()
+   
+    try{
+      await axios.post(endpoint,{
+        name:name,
+        email:email,
+        password:password
+    })
   }
+  catch(e){
+    console.log(e.response.data.errors)
+  }
+    
+    navigate('/')
+}
+  // const Register = async (e) =>{ 
+  //   console.log(e)
+  //   await axios.post(endpoint,{
+      
+  //         name:e.name,
+  //         email:e.email,
+  //         password:e.password,
+          
+  //     })
+  //     navigate('/show_category')
+  // }
   return (
     <>
     <NavBar/>
-    <form onSubmit={ handleSubmit(Register) } className='form-react'>
+    <form style={{marginLeft: '20px',marginTop:'20px'}} onSubmit={Register}>
+            <div className='mb-3'>
+                <label className='form-label'>Nombre</label>
+                <input
+                value={name}
+                onChange={(e)=> setName(e.target.value)}
+                type='text'
+                className='form-control'
+                />
+            </div>
+            <div className='mb-3'>
+                <label className='form-label'>Email</label>
+                <input
+                value={email}
+                onChange={(e)=> setEmail(e.target.value)}
+                type='text'
+                className='form-control'
+                />
+            </div>
+            <div className='mb-3'>
+                <label className='form-label'>Password</label>
+                <input
+                value={password}
+                onChange={(e)=> setPassword(e.target.value)}
+                type='text'
+                className='form-control'
+                />
+            </div>
+
+
+            <div style={{ display: 'inline-block', marginLeft: '10px',marginTop:'20px' }}>
+                <ColorButtons
+                type='submit'
+                tittle='Guardar'/>
+             </div>
+             <div onClick={()=>navigate(-1)} style={{ display: 'inline-block', marginLeft: '10px',marginTop:'20px' }}>
+                
+                    <ColorButtons
+                    tittle='Descartar'/>
+                
+            </div>
+            
+           
+           
+
+        </form>
+    {/* <form onSubmit={ handleSubmit(Register) } className='form-react'>
     <div className='form-control'>
         <label>
            Name
@@ -53,7 +122,7 @@ function Register() {
     </div>
 
     <button type='submit'>Send</button>
-</form>
+</form> */}
 </>
   )
 }
